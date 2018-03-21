1c5c903b9cd3b09ce56c61ac7e66407d24729697
Calypte <calypte42@gmail.com>
2018-03-21 17:18:24 +0100

    Ajout + modif de bdd.php (séparation des login dans un autre fichier) Modif authentification.php (utilise maintenant la base de données)

--- authentification.php
+++ authentification.php
@@ -1,34 +1,43 @@
 <?php
-include 'BDD/bdd.php';
-$bdd=connexionbd();
 
-?>
-
-<?php
+require_once 'BDD/bdd.php';
+$bdd = connexionbd();
 
 // Web Service pour verification de l'authentification
 
 session_start(); // demarrage d'une session
-	
-$login=$_GET['login'];
-$mdp=$_GET['mot_de_passe'];
-$reponse=false;
-$donneesJson = file_get_contents('authentificatio.json'); // Récupère le contenu du fichier .json sous forme d'une chaine de caractères
-$array = json_decode($donneesJson, true); // Transforme en tableau
 
-foreach($array as $log=>$tab){
-	if ($log == $login){
-		if($tab['mot_de_passe'] == $mdp){
-			$reponse=true;
-		}
-	}
-}	
+$login = $_GET['login'];
+$mdp = $_GET['mdp'];
 
-if ($reponse == true){
-	$_SESSION[$login] = 1; // 1 pour connecté, 0 pour déconnecté
+$requete = "SELECT mdp from compte WHERE pseudo='$login'";  // On prepare une requete permettant de recupere l'ensemble des valeurs qui nous interessent
+$value = requete($bdd,$requete); // value recupere la reponse de la requete
+
+// Récuperation du mot de passe si utilisateur existant
+if ($value) {
+    $mdpUtilisateur = $value[0]['mdp'];
+} else {
+    $mdpUtilisateur = null;
+}
+
+// Les deux opérations suivantes servent à fermer la connexion à la requête et
+// à la base de données
+$value = null;
+$bdd = null;
+
+if ($mdp == $mdpUtilisateur) {
+    $_SESSION[$login] = 1; // 1 pour connecté, 0 pour déconnecté
+    header('Refresh: 5; URL=ajoutGrotte.php');
+    echo "Authentification réussie ! Redirection dans 5 secondes...";
+    /*
 	http_response_code(200); // succès de la requête
-}else{
+    */
+} else {
+    header('Refresh: 5; URL=index.php');
+    echo "Echec de l'authentification ! Redirection dans 5 secondes...";
+    /*
 	http_response_code(401); // utilisateur non authentifié
+    */
 }
 
 ?>
