80e889784d049157295e1effbde50c254f890162
dmasclef <diane.masclef@gmail.com>
2018-03-22 11:27:24 +0100

    ajout page apres connexion,html css barre navigation/ menu vertical

---
+++ accueil.php
@@ -0,0 +1,17 @@
+<!Doctype html>
+
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<title>Accueil</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"><!-- style Bootstrap v3.3.7 -->
+		<link rel="stylesheet" href="CSS/entete.css">
+		<link rel="stylesheet" href="" type="text/css">
+	</head>
+
+	<body>	
+<?php
+include'HTML/entete.html';
+?>
+	</body>
+</html>
---
+++ menuVerticalConsultation.php
@@ -0,0 +1,61 @@
+<!Doctype html>
+
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"><!-- style Bootstrap v3.3.7 -->
+ 		<link rel="stylesheet" href="CSS/entete.css">
+		<link rel="stylesheet" href="CSS/menuVertical.css" type="text/css">
+
+	</head>
+
+	<body>	
+<?php
+include'HTML/entete.html';
+?>
+
+	</br>
+	</br>
+	</br>
+	</br>
+		<!-- ONGLETS NAVIGATION VERTICALE GAUCHE -->
+		
+		<div id='menuVertical'>
+			<ul>
+			   <li><a href='#'><span>Grotte</span></a></li>
+			   </br>
+			   <li><a href='#'><span>Site</span></a></li>
+			   </br>
+			   <li><a href='#'><span>Piège</span></a></li>
+			   </br>
+			   <li class='active has-sub'><a href=''><span>Individu</span></a>
+			      <ul>
+				 <li class='sousMenu'><a href=''><span>Gène</span></a></li>
+				 <li class='sousMenu1'><a href=''><span>Taxonomie</span></a></li>
+			      </ul>
+			   </li>
+			</ul>
+		</div>
+
+
+		<!-- BARRE RECHERCHE -->
+		
+		<form class="navbar-form" role="search">
+			<div class="input-group">
+				<input class="form-control" type="text" placeholder="Rechercher..." size="30">
+				<div class="input-group-btn">
+					<button class="btn btn-default" type="submit">
+					<i class="glyphicon glyphicon-search"></i>
+					</button>
+				</div>
+			</div>
+		</form>
+		
+		<!--<div id="barreDeRecherche">
+		    <form action="">
+			<input class="champ" type="text" placeholder="Recherche..."/>
+			<button class = "iconeRecherche" type="submit"><img src="IMAGES/recherche.png" height="32"/></button>
+		    </form>
+		</div>-->
+	</body>
+</html>
